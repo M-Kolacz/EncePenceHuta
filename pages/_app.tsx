934db321b95776bbe07abddccd6f326fe6668862
M-Kolacz <michal.kolacz45@gmail.com>
2021-03-15 09:34:47 +0100

    Integrate Material-UI with project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import theme from 'shared/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    useEffect(() => {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+    return (
+        <>
+            <Head>
+                <title>My page</title>
+                <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default MyApp;
