fb6014f4b195acf21ae81b2b25db5165c54d1b63
Michal Kolacz <michal.kolacz45@gmail.com>
2022-01-21 20:21:07 +0100

    feat: add new charges

--- src/components/Offer/Table/TabPanels/Charges/index.tsx
+++ src/components/Offer/Table/TabPanels/Charges/index.tsx
@@ -1,5 +1,7 @@
 import { Typography } from '@material-ui/core';
 
+import { charges } from '../../../../../shared/SSOT/ChargesData';
+
 import useStyles from '../TabPanelsStyles';
 
 export interface ChargesProps {}
@@ -11,14 +13,31 @@ export const Charges: React.FC<ChargesProps> = () => {
             <Typography variant='h3' component='h2' className={classes.Title}>
                 Abonament
             </Typography>
+            <Typography variant='body1'>
+                Opłata za żłobek w 2022 roku wynosi 1195 zł (kwotę pomniejszamy o dotację z Urzędu Miasta Krakowa i
+                ewentualnie dofinansowanie do opieki w złobku w wysokości 400zł.)
+            </Typography>
+            <Typography variant='h3' component='h2' className={classes.Title}>
+                Wysokość dotacji UMK (1.70zł do każdej godziny pobytu dziecka)
+            </Typography>
             <ul>
-                <li>
-                    <Typography variant='body1'> do 10 godzin dziennie - ok. 500 zł</Typography>
-                </li>
-                <li>
-                    <Typography variant='body1'> do 5 godzin dziennie - ok. 400 zł</Typography>
-                </li>
+                {charges.map((charge) => (
+                    <li key={charge.month} style={{ listStyleImage: 'url(images/png/star1.png)', padding: '10px 0' }}>
+                        <Typography variant='h5' style={{ fontSize: '30px' }}>
+                            {charge.month}
+                        </Typography>
+                        <ul style={{ padding: '10px 0' }}>
+                            <li style={{ listStyleImage: 'initial', padding: '5px 0' }}>
+                                <Typography variant='body1'>{charge.main}</Typography>
+                            </li>
+                            <li style={{ listStyleImage: 'initial', padding: '5px 0' }}>
+                                <Typography variant='body1'>{charge.secondary}</Typography>
+                            </li>
+                        </ul>
+                    </li>
+                ))}
             </ul>
+
             <Typography variant='h3' component='h2' className={classes.Title}>
                 Dane do przelewu
             </Typography>
---
+++ src/shared/SSOT/ChargesData.ts
@@ -0,0 +1,14 @@
+export const charges = [
+    { month: 'Styczeń 20 dni', main: '10 godzin 340 zł', secondary: '5 godzin 170 zł' },
+    { month: 'Luty 20 dni', main: '10 godzin 340 zł', secondary: '5 godzin 170 zł' },
+    { month: 'Marzec 23 dni', main: '10 godzin 391 zł', secondary: '5 godzin 195.50 zł' },
+    { month: 'Kwiecień 20 dni', main: '10 godzin 340 zł', secondary: '5 godzin 170 zł' },
+    { month: 'Maj 21 dni', main: '10 godzin 357 zł', secondary: '5 godzin 178.50 zł' },
+    { month: 'Czerwiec 21 dni', main: '10 godzin 357 zł', secondary: '5 godzin 178.50 zł' },
+    { month: 'Lipiec 21 dni', main: '10 godzin 357 zł', secondary: '5 godzin 178.50 zł' },
+    { month: 'Sierpień 22 dni', main: '10 godzin 374 zł', secondary: '5 godzin 187 zł' },
+    { month: 'Wrzesień 22 dni', main: '10 godzin 374 zł', secondary: '5 godzin 187 zł' },
+    { month: 'Październik 21 dni', main: '10 godzin 357 zł', secondary: '5 godzin 178.50 zł' },
+    { month: 'Listopad 20 dni', main: '10 godzin 340 zł', secondary: '5 godzin 170 zł' },
+    { month: 'Grudzień 21 dni', main: '10 godzin 357 zł', secondary: '5 godzin 178.50 zł' },
+];
